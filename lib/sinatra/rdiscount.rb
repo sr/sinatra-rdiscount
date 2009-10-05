2a2c53086b81357de90c1c5b9d4ec5e47d1c6026
Simon Rozet <simon@rozet.name>
2009-10-05 22:37:28 +0200

    Use tilt's RDiscount implementation

--- lib/sinatra/rdiscount.rb
+++ lib/sinatra/rdiscount.rb
@@ -4,28 +4,9 @@ require 'sinatra/base'
 
 module Sinatra
   module RDiscount
-
     def rdiscount(template, options={}, locals={})
-      render :rdiscount, template, options, locals
+      render :markdown, template, options, locals
     end
-
-  private
-
-    def render_rdiscount(data, options, locals, &block)
-      if block_given?
-        # render layout
-        instance = ::Erubis::Eruby.new(data)
-      else
-        # render template
-        markdown = ::RDiscount.new(data)
-        html = markdown.to_html
-        instance = ::Erubis::Eruby.new(html, :pattern => '\{% %\}')
-      end
-      locals_assigns = locals.to_a.collect { |k,v| "#{k} = locals[:#{k}]" }
-      src = "#{locals_assigns.join("\n")}\n#{instance.src}"
-      eval src, binding, '(__ERB__)', locals_assigns.length + 1
-    end
-
   end
 
   helpers RDiscount
